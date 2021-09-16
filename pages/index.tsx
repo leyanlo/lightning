cdac0136bea6c75fd052f6dd4ca767cb9a26109b
Leyan Lo <leyanlo@gmail.com>
2021-09-16 01:12:29 -0700

    chore: update to use generators and stronger types
    Update to use generators and stronger types. Generators will be needed for animations.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,209 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import * as React from 'react';
 
 const width = 64;
 const height = 128;
 const pTop = Math.sqrt(width) / (Math.sqrt(width) + Math.sqrt(height));
-const start = [0, ~~(width / 2)];
+const start: [number, number] = [0, ~~(width / 2)];
 
-function createMaze() {
-  const maze = [];
+enum Kind {
+  Empty,
+  Start,
+  Path,
+  Strike,
+  Flash,
+}
+
+type Walls = {
+  // has top wall
+  top: boolean;
+  // has left wall
+  left: boolean;
+};
+
+type Cell = {
+  walls: Walls;
+} & (
+  | {
+      kind: Kind.Empty;
+    }
+  | {
+      kind: Kind.Start;
+    }
+  | {
+      kind: Kind.Path;
+      // how bright the cell is, from 0 to 1, at most 25% of the height
+      weight: number;
+      // coordinates of the next cell towards the start
+      next: [number, number];
+    }
+  | {
+      kind: Kind.Strike;
+      // coordinates of the next cell towards the start
+      next: [number, number];
+    }
+  | {
+      kind: Kind.Flash;
+    }
+);
+
+// Initialize random maze. Could result in invalid maze with no solution.
+function createRandomMaze(): Cell[][] {
+  const maze: Cell[][] = [];
   for (let i = 0; i < height; i++) {
     maze.push([]);
     for (let j = 0; j < width; j++) {
       const top =
         // avoid closed cells
-        (!maze[i - 1]?.[j].top ||
-          !maze[i - 1]?.[j].left ||
-          !maze[i - 1]?.[j + 1]?.left) &&
+        (!maze[i - 1]?.[j].walls.top ||
+          !maze[i - 1]?.[j].walls.left ||
+          !maze[i - 1]?.[j + 1]?.walls.left) &&
         Math.random() < pTop;
       const left = Math.random() < 1 - pTop;
-      maze[i].push({ top, left });
+      maze[i].push({
+        walls: { top, left },
+        kind: Kind.Empty,
+      });
     }
   }
+  maze[start[0]][start[1]].kind = Kind.Start;
   return maze;
 }
 
-function findStrike(maze) {
-  const queue = [start];
-  while (queue.length) {
-    const [r, c] = queue.shift();
-    // hit bottom
-    if (r === height - 1) {
-      return [r, c];
-    }
-    if (maze[r + 1]?.[c] && !maze[r + 1][c].top && !maze[r + 1][c].next) {
-      maze[r + 1][c].next = [r, c];
-      queue.push([r + 1, c]);
-    }
-    if (maze[r][c + 1] && !maze[r][c + 1].left && !maze[r][c + 1].next) {
-      maze[r][c + 1].next = [r, c];
-      queue.push([r, c + 1]);
-    }
-    if (maze[r - 1]?.[c] && !maze[r][c].top && !maze[r - 1][c].next) {
-      maze[r - 1][c].next = [r, c];
-      queue.push([r - 1, c]);
-    }
-    if (maze[r][c - 1] && !maze[r][c].left && !maze[r][c - 1].next) {
-      maze[r][c - 1].next = [r, c];
-      queue.push([r, c - 1]);
+// Reset maze to empty except for starting cell.
+function resetMaze(maze: Cell[][]): Cell[][] {
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze[i].length; j++) {
+      maze[i][j] = {
+        walls: maze[i][j].walls,
+        kind: Kind.Empty,
+      };
     }
   }
-  // did not hit bottom
-  return null;
+  maze[start[0]][start[1]].kind = Kind.Start;
+  return maze;
+}
+
+// Generator for the path from start to strike cell. Yields lowest coordinates.
+function* pathGenerator(maze: Cell[][]): Generator<[number, number]> {
+  let queue = [start];
+  let strike;
+  while (queue.length && strike?.[0] !== height - 1) {
+    const nextCells = {};
+    strike = queue[0];
+    for (const [r, c] of queue) {
+      if (r > strike[0]) {
+        strike = [r, c];
+      }
+      if (maze[r + 1]?.[c].kind === Kind.Empty && !maze[r + 1][c].walls.top) {
+        nextCells[[r + 1, c].join()] = [r, c];
+      }
+      if (maze[r][c + 1]?.kind === Kind.Empty && !maze[r][c + 1].walls.left) {
+        nextCells[[r, c + 1].join()] = [r, c];
+      }
+      if (maze[r - 1]?.[c].kind === Kind.Empty && !maze[r][c].walls.top) {
+        nextCells[[r - 1, c].join()] = [r, c];
+      }
+      if (maze[r][c - 1]?.kind === Kind.Empty && !maze[r][c].walls.left) {
+        nextCells[[r, c - 1].join()] = [r, c];
+      }
+    }
+
+    const nextQueue = [];
+    for (const key in nextCells) {
+      const [r, c] = key.split(',').map(Number);
+      maze[r][c] = {
+        walls: maze[r][c].walls,
+        kind: Kind.Path,
+        // 100% at strike, lerp to 0 over a quarter of the height
+        weight: Math.max(0, 1 - (4 * (strike[0] - r)) / height),
+        next: nextCells[key],
+      };
+      nextQueue.push([r, c]);
+    }
+    queue = nextQueue;
+    yield strike;
+  }
+}
+
+// Generator for the strike to the start.
+function* strikeGenerator(
+  maze: Cell[][],
+  strike: [number, number]
+): Generator<[number, number]> {
+  let curr = strike;
+  let currCell = maze[curr[0]][curr[1]];
+  while (currCell.kind === Kind.Path) {
+    maze[curr[0]][curr[1]] = {
+      walls: currCell.walls,
+      kind: Kind.Strike,
+      next: currCell.next,
+    };
+    yield curr;
+    curr = currCell.next;
+    currCell = maze[curr[0]][curr[1]];
+  }
+}
+
+// Generator for the flash.
+function* flashGenerator(
+  maze: Cell[][],
+  strike: [number, number]
+): Generator<[number, number]> {
+  let curr = strike;
+  let currCell = maze[curr[0]][curr[1]];
+  while (currCell.kind === Kind.Strike) {
+    maze[curr[0]][curr[1]] = {
+      walls: currCell.walls,
+      kind: Kind.Strike,
+      next: currCell.next,
+    };
+    curr = currCell.next;
+    currCell = maze[curr[0]][curr[1]];
+  }
+  yield curr;
+}
+
+// Infinite loop generator for the path, strike, and flash.
+// noinspection JSUnusedLocalSymbols
+function* lightningGenerator(maze: Cell[][]): Generator<Kind> {
+  while (true) {
+    let strike;
+    for (strike of pathGenerator(maze)) {
+      yield Kind.Path;
+    }
+    for (const _ of strikeGenerator(maze, strike)) {
+      yield Kind.Strike;
+    }
+    for (const _ of flashGenerator(maze, strike)) {
+      yield Kind.Flash;
+    }
+    resetMaze(maze);
+  }
+}
+
+function createMaze(): Cell[][] {
+  let maze = createRandomMaze();
+  let strike;
+  for (strike of pathGenerator(maze)) {
+  }
+  // invalid maze that does not have a path from top to bottom
+  while (strike[0] < height - 1) {
+    maze = createRandomMaze();
+    for (strike of pathGenerator(maze)) {
+    }
+  }
+  return resetMaze(maze);
 }
 
 export const Home = (): JSX.Element => {
-  let maze = createMaze();
-  let strike = findStrike(maze);
-  while (!strike) {
-    maze = createMaze();
-    strike = findStrike(maze);
-  }
+  const maze = React.useMemo(() => {
+    const m = createMaze();
+    for (const _ of pathGenerator(m)) {
+    }
+    return m;
+  }, []);
   return (
     <div className="container">
       <Head>
@@ -77,9 +221,10 @@ export const Home = (): JSX.Element => {
                     key={j}
                     className={[
                       `cell`,
-                      cell.top && '-top',
-                      cell.left && '-left',
-                      cell.next && '-path',
+                      cell.walls.top && '-top',
+                      cell.walls.left && '-left',
+                      cell.kind === Kind.Start && '-start',
+                      cell.kind === Kind.Path && '-path',
                     ]
                       .filter(Boolean)
                       .join(' ')}
@@ -145,6 +290,10 @@ export const Home = (): JSX.Element => {
 
         .cell.-left {
           border-left-width: 1px;
+        }
+
+        .cell.-start {
+          background: #ffd;
         }
 
         .cell.-path {
